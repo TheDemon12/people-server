83460663d4ccc8541e9e29528093dc2039082067
Kartik Bhalla <kartikbhalla12@gmail.com>
2021-10-03 21:10:01 +0530

    initial setup
    Signed-off-by: Kartik Bhalla <kartikbhalla12@gmail.com>

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import cors from 'cors';
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(morgan('tiny'));
+
+app.get('/', (req, res) => {
+	res.send('Hello World');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+	console.log(`Server Running on https://localhost:${PORT}`);
+});
